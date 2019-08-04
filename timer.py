71638d35f65411ad320c823244066dfe33fe8e39
Amin Jun <mohammad.amin.ghiasi@gmail.com>
2019-08-03 20:45:10 -0400

    We have the new python2 terminal timer

---
+++ timer.py
@@ -0,0 +1,57 @@
+#from __future__ import print_function
+import datetime 
+import sys 
+import signal
+import time
+
+class GracefulKiller:
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
+
+
+def load_db():
+    db = {} 
+    with open('log.db', 'r') as f:
+        for line in f: 
+            key, value = line.split('\t')
+            value = int(value)
+            db[key] = value 
+            print '{}:\t{}'.format(key, hms(value))
+    return db
+
+def save_db(db):
+    with open('log.db', 'w') as f:
+        for key, val in db.iteritems():
+            f.write('{}\t{}\n'.format(key, val))
+
+def hms(t): 
+    return '{}:{}:{}'.format(int(t/(60*60)), int(t/60)%60, int(t%60), )
+
+
+if __name__ == '__main__': 
+    if len(sys.argv) < 2:
+        raise Exception('Tell me what you doin')
+
+    category = sys.argv[1]
+    start = datetime.datetime.now()
+
+    killer = GracefulKiller()
+    db = load_db()
+    saved_t = db.get(category, 0)
+    db[category] = saved_t
+    while not killer.kill_now: 
+        end = datetime.datetime.now()
+        time.sleep(1)
+        during = end - start
+        ts = during.total_seconds()
+        print  '\r', '{}:{}\t|total:\t{}'.format(category, hms(ts), hms(ts+saved_t) ), 
+        sys.stdout.flush()
+    db[category] += int(during.total_seconds())
+    save_db(db)
+    print  '\nSaved {}:{}\t|total:\t{}'.format(category, hms(ts), hms(ts+saved_t) ), 
+    
